cbbba1df01f396c0c76e5693a683223dacb07df5
Roman Lapin <lampus.lapin@gmail.com>
2012-02-29 11:08:16 +0400

    Change pin assigment for SDRAM

--- boards/altera/de0/syn/quartus/tcl/VERSATILE_SDRAM_pin_assignments.tcl
+++ boards/altera/de0/syn/quartus/tcl/VERSATILE_SDRAM_pin_assignments.tcl
@@ -1,87 +1,87 @@
-set_location_assignment PIN_P2 -to sdram_a_pad_o[0]
+set_location_assignment PIN_C4 -to sdram_a_pad_o[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[0]
-set_location_assignment PIN_N5 -to sdram_a_pad_o[1]
+set_location_assignment PIN_A3 -to sdram_a_pad_o[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[1]
-set_location_assignment PIN_N6 -to sdram_a_pad_o[2]
+set_location_assignment PIN_B3 -to sdram_a_pad_o[2]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[2]
-set_location_assignment PIN_M8 -to sdram_a_pad_o[3]
+set_location_assignment PIN_C3 -to sdram_a_pad_o[3]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[3]
-set_location_assignment PIN_P8 -to sdram_a_pad_o[4]
+set_location_assignment PIN_A5 -to sdram_a_pad_o[4]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[4]
-set_location_assignment PIN_T7 -to sdram_a_pad_o[5]
+set_location_assignment PIN_C6 -to sdram_a_pad_o[5]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[5]
-set_location_assignment PIN_N8 -to sdram_a_pad_o[6]
+set_location_assignment PIN_B6 -to sdram_a_pad_o[6]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[6]
-set_location_assignment PIN_T6 -to sdram_a_pad_o[7]
+set_location_assignment PIN_A6 -to sdram_a_pad_o[7]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[7]
-set_location_assignment PIN_R1 -to sdram_a_pad_o[8]
+set_location_assignment PIN_C7 -to sdram_a_pad_o[8]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[8]
-set_location_assignment PIN_P1 -to sdram_a_pad_o[9]
+set_location_assignment PIN_B7 -to sdram_a_pad_o[9]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[9]
-set_location_assignment PIN_N2 -to sdram_a_pad_o[10]
+set_location_assignment PIN_B4 -to sdram_a_pad_o[10]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[10]
-set_location_assignment PIN_N1 -to sdram_a_pad_o[11]
+set_location_assignment PIN_A7 -to sdram_a_pad_o[11]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[11]
-set_location_assignment PIN_L4 -to sdram_a_pad_o[12]
+set_location_assignment PIN_C8 -to sdram_a_pad_o[12]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_a_pad_o[12]
 
-set_location_assignment PIN_G2 -to sdram_dq_pad_io[0]
+set_location_assignment PIN_D10 -to sdram_dq_pad_io[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[0]
-set_location_assignment PIN_G1 -to sdram_dq_pad_io[1]
+set_location_assignment PIN_G10 -to sdram_dq_pad_io[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[1]
-set_location_assignment PIN_L8 -to sdram_dq_pad_io[2]
+set_location_assignment PIN_H10 -to sdram_dq_pad_io[2]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[2]
-set_location_assignment PIN_K5 -to sdram_dq_pad_io[3]
+set_location_assignment PIN_E9 -to sdram_dq_pad_io[3]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[3]
-set_location_assignment PIN_K2 -to sdram_dq_pad_io[4]
+set_location_assignment PIN_F9 -to sdram_dq_pad_io[4]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[4]
-set_location_assignment PIN_J2 -to sdram_dq_pad_io[5]
+set_location_assignment PIN_G9 -to sdram_dq_pad_io[5]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[5]
-set_location_assignment PIN_J1 -to sdram_dq_pad_io[6]
+set_location_assignment PIN_H9 -to sdram_dq_pad_io[6]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[6]
-set_location_assignment PIN_R7 -to sdram_dq_pad_io[7]
+set_location_assignment PIN_F8 -to sdram_dq_pad_io[7]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[7]
-set_location_assignment PIN_T4 -to sdram_dq_pad_io[8]
+set_location_assignment PIN_A8 -to sdram_dq_pad_io[8]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[8]
-set_location_assignment PIN_T2 -to sdram_dq_pad_io[9]
+set_location_assignment PIN_B9 -to sdram_dq_pad_io[9]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[9]
-set_location_assignment PIN_T3 -to sdram_dq_pad_io[10]
+set_location_assignment PIN_A9 -to sdram_dq_pad_io[10]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[10]
-set_location_assignment PIN_R3 -to sdram_dq_pad_io[11]
+set_location_assignment PIN_C10 -to sdram_dq_pad_io[11]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[11]
-set_location_assignment PIN_R5 -to sdram_dq_pad_io[12]
+set_location_assignment PIN_B10 -to sdram_dq_pad_io[12]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[12]
-set_location_assignment PIN_P3 -to sdram_dq_pad_io[13]
+set_location_assignment PIN_A10 -to sdram_dq_pad_io[13]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[13]
-set_location_assignment PIN_N3 -to sdram_dq_pad_io[14]
+set_location_assignment PIN_E10 -to sdram_dq_pad_io[14]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[14]
-set_location_assignment PIN_K1 -to sdram_dq_pad_io[15]
+set_location_assignment PIN_F10 -to sdram_dq_pad_io[15]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dq_pad_io[15]
 
-set_location_assignment PIN_R6 -to sdram_dqm_pad_o[0]
+set_location_assignment PIN_E7 -to sdram_dqm_pad_o[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dqm_pad_o[0]
-set_location_assignment PIN_T5 -to sdram_dqm_pad_o[1]
+set_location_assignment PIN_B8 -to sdram_dqm_pad_o[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_dqm_pad_o[1]
 
-set_location_assignment PIN_M7 -to sdram_ba_pad_o[0]
+set_location_assignment PIN_B5 -to sdram_ba_pad_o[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_ba_pad_o[0]
-set_location_assignment PIN_M6 -to sdram_ba_pad_o[1]
+set_location_assignment PIN_A4 -to sdram_ba_pad_o[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_ba_pad_o[1]
 
-set_location_assignment PIN_L1 -to sdram_cas_pad_o
+set_location_assignment PIN_G8 -to sdram_cas_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_cas_pad_o
 
-set_location_assignment PIN_L7 -to sdram_cke_pad_o
+set_location_assignment PIN_E6 -to sdram_cke_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_cke_pad_o
 
-set_location_assignment PIN_P6 -to sdram_cs_n_pad_o
+set_location_assignment PIN_G7 -to sdram_cs_n_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_cs_n_pad_o
 
-set_location_assignment PIN_L2 -to sdram_ras_pad_o
+set_location_assignment PIN_F7 -to sdram_ras_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_ras_pad_o
 
-set_location_assignment PIN_C2 -to sdram_we_pad_o
+set_location_assignment PIN_D6 -to sdram_we_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_we_pad_o
 
-set_location_assignment PIN_R4 -to sdram_clk_pad_o
+set_location_assignment PIN_E5 -to sdram_clk_pad_o
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to sdram_clk_pad_o
