d4ad1a9b9e27a49242f9ddae96b4ea446c0b2836
Roman Lapin <lampus.lapin@gmail.com>
2012-02-29 12:02:42 +0400

    Change pin assigment for GENERIC_JTAG_TAP

--- boards/altera/de0/syn/quartus/tcl/GENERIC_JTAG_TAP_pin_assignments.tcl
+++ boards/altera/de0/syn/quartus/tcl/GENERIC_JTAG_TAP_pin_assignments.tcl
@@ -1,8 +1,8 @@
-set_location_assignment PIN_A14 -to tck_pad_i
+set_location_assignment PIN_AA20 -to tck_pad_i #Dummy - GPIO1_D[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tck_pad_i
-set_location_assignment PIN_C14 -to tdo_pad_o
+set_location_assignment PIN_AB20 -to tdo_pad_o #Dummy - GPIO1_D[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdo_pad_o
-set_location_assignment PIN_B16 -to tdi_pad_i
+set_location_assignment PIN_AA19 -to tdi_pad_i #Dummy - GPIO1_D[2]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tdi_pad_i
-set_location_assignment PIN_C15 -to tms_pad_i
+set_location_assignment PIN_AB19 -to tms_pad_i #Dummy - GPIO1_D[3]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to tms_pad_i
